dabc6411e2c5107f758ee307074f33ee22d2df84
Daniil <daniilafendulov@bk.ru>
2022-08-18 17:13:45 +0300

    Add base pattern

---
+++ src/components/TaskEdit.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// блок для редактирования задачи
+function TaskEdit() {
+  return (
+    <div className='task-edit'>TaskEdit</div>
+  )
+}
+
+export default TaskEdit
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// блок со списком задач
+function TaskList() {
+  return (
+    <div className='task-list'>TaskList</div>
+  )
+}
+
+export default TaskList
